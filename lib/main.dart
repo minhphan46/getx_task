15e13bac82fe80ae3d3847ff1d930110f5f794f3
minhphan46 <21522345@gm.uit.edu.vn>
2023-01-29 14:39:06 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_task/app/modules/home/view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'Todo List using GetX',
+      home: HomeScreen(),
+    );
+  }
+}
